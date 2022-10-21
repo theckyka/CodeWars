f231b36055aa21b99257efe81e279b6fcb27785d
theckyka <theckyka@gmail.com>
2022-10-21 22:34:15 +0300

    added new codewars challenge
    added new codewars challenge

---
+++ 8kyu/basic-mathematical-operations.js
@@ -0,0 +1,32 @@
+/**DESCRIPTION:
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+ */
+
+// My solution
+
+function basicOp(o, v1, v2)
+{
+  let s = 0;
+  if(o === '+') {
+    s = v1 + v2;
+    return s;
+  }else if(o === '-') {
+    s = v1 - v2;
+    return s;
+  }else if(o === '*'){
+    s = v1 * v2;
+    return s;
+  }else if(o === '/') {
+    s = v1 / v2;
+    return s;
+  }
+}
