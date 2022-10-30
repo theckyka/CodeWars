2963c3699780d9d62439201c6cf0e3c18fdbaf0c
TheCkyka <theckyka@gmail.com>
2022-10-30 18:09:57 +0300

    added new codewars challenge
    added new codewars challenge

---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,12 @@
+/**DESCRIPTION:
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters. */
+
+// My solution
+
+function opposite(number) {
+    if(number > 0) {
+      return -number;
+    }else {
+      return -number;
+    }
+  }
